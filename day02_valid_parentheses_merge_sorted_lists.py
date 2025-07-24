19f8ab1ff4bd71a6df2d9c927d4194f60b7a2f07
Techcoder <techversenet7@gmail.com>
2025-07-24 23:49:11 +0530

    Add files via upload

---
+++ day02_valid_parentheses_merge_sorted_lists.py
@@ -0,0 +1,41 @@
+# Day 2 of #30DaysOfDSA
+# Problem 1: Valid Parentheses
+# Problem 2: Merge Two Sorted Linked Lists
+# Language: Python
+
+# Problem 1: Valid Parentheses
+
+def isValid(s):
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+    for char in s:
+        if char in mapping:
+            top = stack.pop() if stack else '#'
+            if mapping[char] != top:
+                return False
+        else:
+            stack.append(char)
+    return not stack
+
+
+# Problem 2: Merge Two Sorted Linked Lists
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def mergeTwoLists(list1, list2):
+    dummy = ListNode()
+    current = dummy
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+
+    current.next = list1 if list1 else list2
+    return dummy.next
