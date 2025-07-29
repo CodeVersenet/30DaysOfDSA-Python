3e2d115568a7845d3e6ca856170625d85c27772e
Techcoder <techversenet7@gmail.com>
2025-07-29 23:28:04 +0530

    Add files via upload

---
+++ Day6_DSA _Challenge.py
@@ -0,0 +1,35 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if isBadVersion(mid):
+                right = mid  
+            else:
+                left = mid + 1  
+
+        return left  
+    
+# Problem 69- Sqrt(x)
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+
+        left, right = 1, x // 2
+
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right  # right will be the integer part of sqrt(x)
